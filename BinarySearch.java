53325107093f2e279835d7f564fe3db15a6e48dd
arjunkhatipatel <mr.arjunkhatipatel@gmail.com>
2024-09-11 23:46:27 +0530

    Binary search recursive and optimized binary search iterative

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,25 +1,46 @@
 public class BinarySearch {
-        public static void main(String[] args){
-            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,};
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,};
 
-            if(binarySearch(arr, 5)){
-                System.out.println("5 is in array.");
-            }
+        if (binarySearch(arr, 5)) {
+            System.out.println("5 is in array.");
         }
 
-        private static boolean binarySearch(int[] arr, int target){
-            int start = 0;
-            int end = arr.length;
-            int mid = (start + end) / 2;
-            for (int i = 0; i < arr.length / 2; i++){
-                if (arr[mid] == target){
-                    return true;
-                }else if (arr[mid] > target){
-                    end = mid;
-                }else {
-                    start = mid;
-                }
-            }
-            return false;
+        if (binarySearchRecusive(arr, 0, 9, 8)) {
+            System.out.println("8 is in array");
         }
     }
+
+    private static boolean binarySearchRecusive(int[] arr, int start, int end, int target) {
+        if (start > end) {
+            return false;
+        }
+
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) {
+            return true;
+        } else if (arr[mid] > target) {
+            end = mid;
+            return binarySearchRecusive(arr, start, end - 1, target);
+        } else {
+            start = mid;
+            return binarySearchRecusive(arr, start + 1, end, target);
+        }
+    }
+
+    private static boolean binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
